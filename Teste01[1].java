6c00c45be75fda179719c4821dcab1cfe5e9de35
Alex Bruno de Oliveira <alexbrunoliveira@gmail.com>
2022-07-22 17:40:01 -0300

    Array de String

---
+++ Teste01[1].java
@@ -0,0 +1,33 @@
+package Colecoes;
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class Teste01 {
+
+  public static void main(String[] args) {
+  
+  List<String> diasSemana = new ArrayList<String>();
+  
+  diasSemana.add("Domingo");
+  
+  diasSemana.add("Segunda - feira");
+  
+  diasSemana.add("Terça- feira");
+  
+  diasSemana.add("Quarta - feira");
+  
+  diasSemana.add("Quinta - fwira ");
+  
+  diasSemana.add("Sexta - feira");
+  
+  diasSemana.add("Sábado");
+  
+  
+  System.out.println(diasSemana);
+  
+  
+  
+   
+  }
+}
