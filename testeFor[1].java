67d49b766ae58aaf4f5a7896ea227e025226a5e2
Alex Bruno de Oliveira <alexbrunoliveira@gmail.com>
2022-07-22 17:38:30 -0300

    Simples comando For

---
+++ testeFor[1].java
@@ -0,0 +1,15 @@
+package com.livro;
+
+public class testeFor {
+
+  public static void main(String[] args) {
+     
+     for (int x = 0; x < 3; x++) {
+     
+       System.out.println("in for loop");
+       break ; // para o looping
+     }
+ 
+     }
+     }
+
